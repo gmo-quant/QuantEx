8fea597d0628cf3a1728d21846a2de7b3b6919c6
Guangzhuan <quan.fin.mo@gmail.com>
2018-05-19 12:35:31 -0400

    KnockoutOption is inherited from ContinuousTimeOptionBase, it doesn't generate object directly

---
+++ KnockoutOption.cpp
@@ -0,0 +1,1 @@
+#include"KnockoutOption.h"
---
+++ KnockoutOption.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ContinuousTimeOptionBase.h"
+
+/**
+ * an option with a barrier
+ */
+
+class KnockoutOption : 
+	public: ContinuousTimeOptionBase{
+public:
+	virtual ~KnockoutOption(){}
+
+	double barrier() const{ return _barrier;}
+	double barrier(double barrier){_barrier = barrier;}
+	bool isPathDependent() const{return true;}
+
+private:
+	double _barrier;
+}
